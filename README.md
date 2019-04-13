8ea83bad778a6bf0fdd8df4fddc62aeb88def8f5
John Wicks <jwwicks@gmail.com>
2019-04-13 12:47:10 -0700

    Initial commit

--- README.md
+++
@@ -1,1 +0,0 @@
-jwwicks.github.io
---
+++ js/main.js
@@ -0,0 +1,55 @@
+debounce = function debounce(func, wait, immediate) {
+  var timeout;
+  return function() {
+    var context = this, args = arguments;
+    var later = function() {
+      timeout = null;
+      if (!immediate) func.apply(context, args);
+    };
+    var callNow = immediate && !timeout;
+    clearTimeout(timeout);
+    timeout = setTimeout(later, wait);
+    if (callNow) func.apply(context, args);
+  };
+};
+poll = function poll(fn, callback, errback, timeout, interval) {
+    var endTime = Number(new Date()) + (timeout || 2000);
+    interval = interval || 100;
+
+    (function p() {
+            // If the condition is met, we're done!
+            if(fn()) {
+                callback();
+            }
+            // If the condition isn't met but the timeout hasn't elapsed, go again
+            else if (Number(new Date()) < endTime) {
+                setTimeout(p, interval);
+            }
+            // Didn't match and too much time, reject!
+            else {
+                errback(new Error('timed out for ' + fn + ': ' + arguments));
+            }
+    })();
+}
+once = function once(fn, context) {
+  var result;
+
+  return function() {
+    if(fn) {
+      result = fn.apply(context || this, arguments);
+      fn = null;
+    }
+
+    return result;
+  };
+}
+getAbsoluteUrl = (function() {
+  var a;
+
+  return function(url) {
+    if(!a) a = document.createElement('a');
+    a.href = url;
+
+    return a.href;
+  };
+})();
